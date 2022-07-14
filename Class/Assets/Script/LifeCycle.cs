add788d942dfc074269ebd7fca0c6e7bca4ab697
jhw030329 <jhw030329@naver.com>
2022-07-14 19:47:57 +0900

    Remote Git
    3일차

--- Class/Assets/Script/LifeCycle.cs
+++ Class/Assets/Script/LifeCycle.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Data
+{
+    // 변수
+    // 기본 접근 지정자는 private 입니다.
+    public int price;
+    public string name;
+    public Sprite sprite;
+
+    // 기본 생성자 
+    // 생성자를 선언하지 않으면 자동으로 기본 생성자를 생성시켜줍니다.
+
+}
 
 public class LifeCycle : MonoBehaviour
 {
-    int health = 0;
+    // 유니티에서는 소멸자가 존재하지 않습니다.
+    public Data [] data;
+
+    public Text[] priceUI;
+    public Text[] nameUI;
+    public Image[] spriteUI;
 
     private void Awake() // <- 1번째
     {
-        Debug.Log("Tiger Awake");
+        for(int i = 0; i < data.Length; i++)
+        {
+            priceUI[i].text = data[i].price.ToString();
+            nameUI[i].text = data[i].name;
+            spriteUI[i].sprite = data[i].sprite;
+        }
     }
 
     void Start()
@@ -28,13 +53,6 @@ public class LifeCycle : MonoBehaviour
 
     private void Update()
     {
-        health++;
-
-        Debug.Log(health);
-
-        if(health >= 100)
-        {
-            health = 0;
-        }
+        
     }
 }
